c34b252d3ea9704093cc8b786381ed8f7558fdbc
Bo Mercado <rbomercado1@gmail.com>
2022-01-17 15:52:47 -0800

    Completed changes to align-items

---
+++ prep-javascript-arrays/main.js
@@ -0,0 +1,14 @@
+const languages=['html','css','javascript','react','node','postgres']
+console.log(languages)
+
+let firstElement=languages[0]
+console.log("The firt element in the array is " + firstElement)
+
+let thirdElement=languages[2]
+console.log("The third element in the array is " + thirdElement)
+
+let length=languages.length
+console.log("The length of the array is " + length)
+
+let lastElement=languages[5]
+console.log("The last element in the array is " + lastElement)
